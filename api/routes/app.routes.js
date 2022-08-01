da6503bd39f622d1649a07492346fafba669ec9a
032AbhishekChoksi <19bmiit032@gmail.com>
2022-08-01 17:54:31 +0530

    Update Restaurant Basic Details by Partner

--- api/routes/app.routes.js
+++ api/routes/app.routes.js
@@ -7,7 +7,11 @@ const router = express.Router();
 // Create a new Restaurant
 router.post("/restaurant", restaurantController.create);
 // Attach Restaurant Document and Other Details Add
-router.put("/restaurant/document/:id", restaurantController.attachDocument);
+router.put("/restaurant/attachdocument/:id", restaurantController.attachDocument);
+// Update Restaurant Basic Details by Partner
+router.put("/restaurant/:restaurantId", restaurantController.updateBasicDetailsByPartner);
+// Update Restaurant Document by Admin
+// router.put("/restaurant/updatedocument/:restaurantId", restaurantController.);
 
 /* Cuisines Route */
 // Create a new Cuisines
