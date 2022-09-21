f98f42e8017bfbdcad0cc7f9edb2100b3125f80e
patel meet <19bmiit0015@gmail.com>
2022-09-21 12:11:36 +0530

    UseCase Diagram is completed and Registration From Design also completed

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Registration from './component/pages/partner/Registration';
+
+function App() {
+  return (
+    <>
+      <Registration/>
+    </>
+  );
+}
+
+export default App;
