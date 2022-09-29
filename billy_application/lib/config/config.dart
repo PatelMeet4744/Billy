2b8de3f05e147590119a74d4b287ce5be56abceb
032AbhishekChoksi <19bmiit032@gmail.com>
2022-09-29 12:25:45 +0530

    Add Some Resources

--- billy_application/lib/config/config.dart
+++ billy_application/lib/config/config.dart
@@ -1,7 +1,7 @@
 class Config {
   static const String appName = "Billy App";
-  static const String apiURL = "172.29.64.1:4000";
-  static const String imageURL = "http://172.29.64.1:4000";
+  static const String apiURL = "192.168.43.155:4000";
+  static const String imageURL = "http://192.168.43.155:4000";
   static const String cuisinesAPI = "api/cuisines";
   static const int pageSize = 10;
   static const String currency = "₹";
