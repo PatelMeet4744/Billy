f8670ede7c50ad206967635a0cd8051e0475bed2
032AbhishekChoksi <19bmiit032@gmail.com>
2023-04-27 01:05:05 +0530

    variant functionality added in Billy App

--- billy_application/lib/pages/item/variant_list_page.dart
+++ billy_application/lib/pages/item/variant_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:billy_application/base/no_data_page.dart';
 import 'package:billy_application/controllers/item_controller.dart';
+import 'package:billy_application/models/item_model.dart';
+import 'package:billy_application/pages/item/item_addon_list_page.dart';
 import 'package:billy_application/utils/colors.dart';
 import 'package:billy_application/utils/dimensions.dart';
 import 'package:billy_application/widgets/app_icon.dart';
@@ -8,9 +10,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class VariantListPage extends StatefulWidget {
-  final int i, j;
+  final List<Item>? itemList;
+  final int? index;
 
-  const VariantListPage({Key? key, required this.i, required this.j})
+  const VariantListPage({Key? key, this.itemList, this.index})
       : super(key: key);
 
   @override
@@ -18,53 +21,82 @@ class VariantListPage extends StatefulWidget {
 }
 
 class _VariantListPageState extends State<VariantListPage> {
-  late int selectedIndex;
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      selectedIndex = 0;
-    });
-  }
-
+  int selected = 0;
   @override
   Widget build(BuildContext context) {
+    return SizedBox(
+      height: Dimensions.screenHeight / 2,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: BigText(
+              text: widget.itemList![widget.index!].itemName!,
+              color: AppColors.mainColor,
+            ),
+            trailing: GestureDetector(
+              onTap: () => Navigator.of(context).pop(),
+              child: const AppIcon(
+                icon: Icons.clear,
+                iconColor: Colors.white,
+                backgroundColor: AppColors.mainColor,
+              ),
+            ),
+          ),
+          Divider(
+            color: Colors.grey[110],
+            thickness: 2.0,
+            indent: 5.0,
+            endIndent: 5.0,
+          ),
+          SizedBox(
+            height: Dimensions.height10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0),
+            child: BigText(
+              text: widget.itemList![widget.index!].variant![0].variantuom!,
+              size: Dimensions.font18,
+            ),
+          ),
+          SizedBox(
+            height: Dimensions.height10,
+          ),
+          Container(
+            color: Colors.white,
+            child: variantListBottomSheet(
+                context, widget.itemList![widget.index!].variant!),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget variantListBottomSheet(BuildContext context, List<Variant>? variant) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         GetBuilder<ItemController>(builder: (item) {
-          return item.itemList[widget.i].item![widget.j].variant!.isNotEmpty
+          return variant!.isNotEmpty
               ? item.isLoaded
                   ? ListView.builder(
                       scrollDirection: Axis.vertical,
                       shrinkWrap: true,
-                      itemCount: item
-                          .itemList[widget.i].item![widget.j].variant!.length,
-                      itemBuilder: (context, k) {
+                      itemCount: variant.length,
+                      itemBuilder: (context, i) {
                         return RadioListTile(
                           title: Text(
-                              "${item.itemList[widget.i].item![widget.j].variant![k].variantName!}  (₹${item.itemList[widget.i].item![widget.j].variant![k].variantSalesPrice!})"),
-                          value: k,
-                          groupValue: item.itemList[widget.i].item![widget.j]
-                              .variant![k].variantuom,
+                              "${variant[i].variantName!}  (₹${variant[i].variantSalesPrice!})"),
+                          value: i,
+                          groupValue: selected,
                           onChanged: ((value) {
                             setState(() {
-                              selectedIndex = value! as int;
+                              selected = value!;
                             });
                           }),
-                          selected: k == selectedIndex ? true : false,
                         );
-                        // return CheckboxListTile(
-                        //   title: Text(item.itemList[widget.i].item![widget.j]
-                        //       .variant![k].variantName!),
-                        //   value: checkedItems[k],
-                        //   onChanged: (newValue) {
-                        //     setState(() {
-                        //       checkedItems[k] = newValue ?? false;
-                        //     });
-                        //   },
-                        // );
                       },
                     )
                   : const CircularProgressIndicator(
@@ -78,6 +110,7 @@ class _VariantListPageState extends State<VariantListPage> {
           padding: const EdgeInsets.only(left: 5.0, right: 2.0, top: 8.0),
           color: Colors.grey[100],
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Divider(
                 color: Colors.grey[110],
@@ -85,21 +118,36 @@ class _VariantListPageState extends State<VariantListPage> {
                 indent: 5.0,
                 endIndent: 5.0,
               ),
-              BigText(
-                text:
-                    "Order Price = ₹${Get.find<ItemController>().itemList[widget.i].item![widget.j].variant![selectedIndex].variantSalesPrice!}",
-                size: Dimensions.font16,
+              SizedBox(height: Dimensions.height10),
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: RichText(
+                    text: TextSpan(
+                  style: TextStyle(
+                    fontSize: Dimensions.font14,
+                    color: AppColors.mainBlackColor,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(text: 'Purchase Price: '),
+                    TextSpan(
+                        text: "₹${variant![selected].variantPrice!}",
+                        style: const TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                )),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  BigText(
-                    text: "Step1/3",
-                    size: Dimensions.font16,
-                    color: AppColors.mainBlackColor,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: BigText(
+                      text: "Step1/3",
+                      size: Dimensions.font16,
+                      color: AppColors.mainBlackColor,
+                    ),
                   ),
                   Container(
-                    width: Dimensions.height40,
+                    width: Dimensions.height50,
                     height: Dimensions.height40,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.horizontal(
@@ -109,56 +157,12 @@ class _VariantListPageState extends State<VariantListPage> {
                         color: AppColors.mainColor),
                     child: GestureDetector(
                       onTap: () {
-                        // Navigator.pop(context);
                         // Item Addon List Page
                         Get.bottomSheet(
-                          SizedBox(
-                            height: Dimensions.screenHeight / 2,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                ListTile(
-                                  title: BigText(
-                                    text: "Item",
-                                    color: AppColors.mainColor,
-                                  ),
-                                  trailing: GestureDetector(
-                                    onTap: () => Navigator.pop(context),
-                                    child: const AppIcon(
-                                      icon: Icons.clear,
-                                      iconColor: Colors.white,
-                                      backgroundColor: AppColors.mainColor,
-                                    ),
-                                  ),
-                                ),
-                                Divider(
-                                  color: Colors.grey[110],
-                                  thickness: 2.0,
-                                  indent: 5.0,
-                                  endIndent: 5.0,
-                                ),
-                                SizedBox(
-                                  height: Dimensions.height10,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 5.0),
-                                  child: BigText(
-                                    text: "Add on",
-                                    size: Dimensions.font18,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: Dimensions.height10,
-                                ),
-                                Container(
-                                  color: Colors.white,
-                                  child: Container(
-                                    child: Text("List of item addodn"),
-                                  ),
-                                )
-                              ],
-                            ),
+                          ItemAddonListPage(
+                            itemList: widget.itemList,
+                            index: widget.index,
+                            selectedVariant: selected,
                           ),
                           elevation: 20.0,
                           isDismissible: false,
@@ -170,13 +174,11 @@ class _VariantListPageState extends State<VariantListPage> {
                             ),
                           ),
                         );
-
-                        // Get.toNamed(RouteHelper.getItemAddonListPage());
                       },
                       child: Icon(
                         Icons.navigate_next_rounded,
                         color: Colors.white,
-                        size: Dimensions.font18,
+                        size: Dimensions.font26,
                       ),
                     ),
                   ),
